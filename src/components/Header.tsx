64ecffbab9597c9fcb474b80e3de940b1ad662ef
Peng Ni <72177522+1128-pni@users.noreply.github.com>
2024-12-15 22:34:51 +0800

    feat: update website title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header: FC = () => {
   return (
     <header className="text-center mb-12 py-8">
       <h1 className="text-3xl font-semibold text-gray-900">
-        Fetal Echocardiography Calculator
+        Fetal Echocardiography Z-score Calculator
       </h1>
       <p className="mt-2 text-gray-600">
         Please enter parameters for calculation
